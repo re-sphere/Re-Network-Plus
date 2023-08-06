6df0b663266c690c3be83905e69af1e79b183b17
ありす <136943652+devaix@users.noreply.github.com>
2023-08-06 23:36:06 +0900

    URLを変更

--- packages/backend/migration/1557761316509-AddSomeUrls.js
+++ packages/backend/migration/1557761316509-AddSomeUrls.js
@@ -6,8 +6,8 @@
 export class AddSomeUrls1557761316509 {
     async up(queryRunner) {
         await queryRunner.query(`ALTER TABLE "meta" ADD "ToSUrl" character varying(512)`);
-        await queryRunner.query(`ALTER TABLE "meta" ADD "repositoryUrl" character varying(512) NOT NULL DEFAULT 'https://github.com/kokonect-link/cherrypick'`);
-        await queryRunner.query(`ALTER TABLE "meta" ADD "feedbackUrl" character varying(512) DEFAULT 'https://github.com/kokonect-link/cherrypick/issues/new'`);
+        await queryRunner.query(`ALTER TABLE "meta" ADD "repositoryUrl" character varying(512) NOT NULL DEFAULT 'https://github.com/re-sphere/re-network-plus'`);
+        await queryRunner.query(`ALTER TABLE "meta" ADD "feedbackUrl" character varying(512) DEFAULT 'https://github.com/re-sphere/re-network-plus/issues/new'`);
     }
     async down(queryRunner) {
         await queryRunner.query(`ALTER TABLE "meta" DROP COLUMN "feedbackUrl"`);
